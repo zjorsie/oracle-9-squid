064f90977a6f75415e9af2891f34afb1620cc33f
zjorsie <zjorsie@users.noreply.github.com>
2023-07-18 17:27:02 +0200

    Update entrypoint.sh
    fix tabs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 if [ $# -eq 0 ]; then
-	    /usr/libexec/squid/cache_swap.sh
-	        source /etc/sysconfig/squid
-		    exec /usr/sbin/squid -N $SQUID_OPTS -f $SQUID_CONF
-	    else
-		        exec $@
+	/usr/libexec/squid/cache_swap.sh
+	source /etc/sysconfig/squid
+	exec /usr/sbin/squid -N $SQUID_OPTS -f $SQUID_CONF
+else
+	exec $@
 fi
 
