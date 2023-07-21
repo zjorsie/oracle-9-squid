fd0152657ab7fd40cad40e845deaa96cd6d55148
Jemoeder <sjorsnijhuis@gmail.com>
2023-07-21 14:28:06 +0200

    made changes to reduce resulting image size

--- build-and-run-v6.sh
+++ build-and-run-v6.sh
@@ -2,8 +2,4 @@
 
 # build container:
 
-docker compose -f ./docker-compose-build-v6.yml build
-docker compose -f ./docker-compose-build-v6.yml down
-docker compose -f ./docker-compose-build-v6.yml up -d
-
-bash ./useful_commands.sh
+docker compose -f ./docker-compose-build-v6.yml build && docker compose -f ./docker-compose-build-v6.yml down && docker compose -f ./docker-compose-build-v6.yml up -d && bash ./useful_commands.sh
--- build-and-run-v7.sh
+++ build-and-run-v7.sh
@@ -2,8 +2,4 @@
 
 # build container:
 
-docker compose -f ./docker-compose-build-v7.yml build
-docker compose -f ./docker-compose-build-v7.yml down
-docker compose -f ./docker-compose-build-v7.yml up -d
-
-bash ./useful_commands.sh
+docker compose -f ./docker-compose-build-v7.yml build && wqdocker compose -f ./docker-compose-build-v7.yml down && docker compose -f ./docker-compose-build-v7.yml up -d && bash ./useful_commands.sh
